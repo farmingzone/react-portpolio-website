627be1aadb519cbd624a1ca07e0deba1e4c087b7
farmingzone <qjawns0404@gmail.com>
2022-07-25 21:46:11 +0900

    1

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href='https://linkedin.com' target="_black"></a>
+        <a href='https://github.com' target="_black"></a>
+        <a href='https://dribbble.com' target="_black"></a>
+        
+    </div>
+  )
+}
+
+export default HeaderSocials
